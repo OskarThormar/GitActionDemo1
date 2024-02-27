b01d396eeb65eb88371c679224224cb96894eecf
Oskar <osth0002@stud.kea.dk>
2024-02-27 10:58:19 +0100

    Initial commit

---
+++ src/main/java/com/example/gitactiondemo1/GitActionDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.gitactiondemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitactiondemo1/GitActionDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitactiondemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
